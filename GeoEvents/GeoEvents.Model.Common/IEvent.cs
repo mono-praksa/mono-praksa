6664e6e6076b0af1564193e6aae406138ed5e5cd
Domagoj Suhić <dsuhic@mathos.hr>
2017-07-04 11:55:32 +0200

    Add model and service interfaces

---
+++ GeoEvents/GeoEvents.Model.Common/IEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoEvents.Model.Common
+{
+    public interface IEvent
+    {
+        #region Properties
+
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Description { get; set; }
+        DateTime StartTime { get; set; }
+        decimal Lat { get; set; }
+        decimal Long { get; set; }
+        long Duration { get; set; }
+        int Category { get; set; }
+        DateTime EndTime { get; set; }
+    }
+}
