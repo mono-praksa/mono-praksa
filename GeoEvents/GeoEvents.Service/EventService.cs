34ddb63fbf3042d4b7400e5a2e7f5fb7b2fcd6c6
Ivana <ivana.pristavnik@gmail.com>
2017-07-04 11:56:19 +0200

    Add model and service

---
+++ GeoEvents/GeoEvents.Service/EventService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoEvents.Service
+{
+    public class EventService : IEventService
+    {
+        #region Properties
+
+        protected IEventRepository Repository { get; set; }
+
+        #endregion Properties
+
+
+
+        #region Constructors
+
+        public EventService(IEventRepository repository)
+        {
+            this.Repository = repository;
+        }
+
+
+        #endregion Constructors
+
+
+
+        #region Methods
+
+        bool CreateEvent(IEvent evt) 
+        {
+            return Repository.CreateEvent(evt);
+        }
+
+        public List<IEvent> GetEvents(IFilter filter)
+        {
+            return Repository.GetEvents(filter);
+        }
+
+        public List<IImage> GetImages(Guid eventId)
+        {
+            return Repository.GetImages(eventId);
+        }
+
+        #endregion Methods
+
+    }
+}
