986bda1ef0df944ff9051d065d4feab6c9e5eda8
Ivana <ivana.pristavnik@gmail.com>
2017-07-04 13:03:01 +0200

    Fixed model ref

--- GeoEvents/GeoEvents.Model.Common/IEvent.cs
+++ GeoEvents/GeoEvents.Model.Common/IEvent.cs
@@ -16,7 +16,6 @@ namespace GeoEvents.Model.Common
         DateTime StartTime { get; set; }
         decimal Lat { get; set; }
         decimal Long { get; set; }
-        long Duration { get; set; }
         int Category { get; set; }
         DateTime EndTime { get; set; }
     }
